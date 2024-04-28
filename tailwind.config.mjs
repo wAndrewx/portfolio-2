665537581772a7fba2e139a7bc3b75db6b04c63e
wAndrewx <andrew/huynh17@gmail.com>
2024-04-28 00:45:25 -0400

    landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    // extend: { fontFamily: { sans: [] } },
+    extend: {
+      fontFamily: {
+        recife: ["RecifeDisplay-Regular"],
+      },
+      animation: {
+        "reverse-spin": "reverse-spin 5s linear infinite",
+      },
+      keyframes: {
+        "reverse-spin": {
+          from: {
+            transform: "rotate(360deg)",
+          },
+        },
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/container-queries")],
 };
