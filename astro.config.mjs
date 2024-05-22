58fd4f3e36222d6043c017ef109d38abad0ceb95
wAndrewx <andrew/huynh17@gmail.com>
2024-05-21 23:05:18 -0400

    packages and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,8 +9,10 @@ export default defineConfig({
   vite: {
     server: {
       watch: {
-        usePolling: true,
-      },
-    },
+        usePolling: true
+      }
+    }
   },
+  output: "server",
+  adapter: cloudflare()
 });
